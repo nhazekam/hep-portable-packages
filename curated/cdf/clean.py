f045acceceb10165733d56f2a99d87ab83d289f5
Nick Hazekamp <nhazekam@nd.edu>
2019-01-22 22:48:54 -0500

    Updates from ShareLaTeX

--- curated/cdf/clean.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import fileinput
-
-for a in fileinput.input():
-    log, count = a.split()
-    log = int(log)
-    count = int(count)
-    print("{} {}".format(2**log, count/1e6))
